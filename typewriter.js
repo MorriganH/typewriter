a40042bf91457bb0dc0e4c41128dd6a5f0229d84
MorriganH <morriganhennessy@gmail.com>
2023-04-11 14:17:15 -0400

    prints each character 50ms apart

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = 'hello there from lighthouse labs';
+
+let ms = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, ms);
+  ms += 50;
+}
+
+setTimeout(() => {
+  console.log();
+}, ms)
